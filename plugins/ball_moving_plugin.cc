b68139f78ed353fe1787e79da17ae1e760914fcb
Mohamed Tahiri <mohamed.tahiri@etu.u-bordeaux.fr>
2022-11-10 17:07:47 +0100

    Ca marche pas help

--- plugins/ball_moving_plugin.cc
+++ plugins/ball_moving_plugin.cc
@@ -3,39 +3,51 @@
 #include <gazebo/physics/physics.hh>
 #include <ignition/math/Vector3.hh>
 #include <string.h>
+#include "ros/ros.h"
+#include <geometry_msgs/Twist.h>
 
 namespace gazebo
 {
+
+    static geometry_msgs::Twist move_ball_callback(const geometry_msgs::Twist& msg)
+    {
+      ROS_INFO_STREAM("Subscriber velocities:" << " linear x y z = " << msg.linear.x  << msg.linear.y << msg.linear.z 
+                      << " angular x y z = " << msg.angular.x << msg.angular.y << msg.angular.z);
+    }
+
   class Ball_moving_plugin : public ModelPlugin
   {
-
-    private :
-    std::string key;
-    float velocity;
-
     // Pointer to the model
     private: physics::ModelPtr model;
     // Pointer to the update event connection
     private: event::ConnectionPtr updateConnection;
 
+    private: std::unique_ptr<ros::NodeHandle> rosNode;
+
+    private: ros::Subscriber rosSub;
+
     public: void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
     {
-        this->model = _model;
+      this->model = _model;
 
-        // Print Model name on console when world is loaded (yellow ball here)
-        std::cout << "Model Name = " << this->model->GetName() << std::endl; 
+      // Print Model name on console when world is loaded (yellow ball here)
+      std::cout << "Model Name = " << this->model->GetName() << std::endl; 
 
-        // Listen to the update event. This event is broadcast every
-        // simulation iteration.
-        this->updateConnection = event::Events::ConnectWorldUpdateBegin(
-                                 std::bind(&Ball_moving_plugin::OnUpdate, this));
+      // Subscribe to the Ball info topic
+      this->rosSub = rosNode.subscribe("ball_moving_info", 1000, move_ball_callback);
+
+      // Listen to the update event. This event is broadcast every
+      // simulation iteration.
+      this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+                                std::bind(&Ball_moving_plugin::OnUpdate, this));
     }
 
     // Called by the world update start event
     public: void OnUpdate()
     {
+      //ros::Subscriber sub = n.subscribe("ball_moving_info", std::string, );
       //apply a small velocity on ball on axis x
-
+      this->model->SetLinearVel(ignition::math::Vector3d(.3, 0, 0));
     }
 
   };
--- src/ball_teleop_key.py
+++ src/ball_teleop_key.py
@@ -3,33 +3,13 @@
 import rospy
 import sys, select, os
 import tty, termios
-from turtlebot3_ball_following.msg import BallMoveInfo
+from geometry_msgs.msg import Twist
 
-msg = """
-=============================
-Moving the ball around:
-        Z
-   Q    S    D
-     
-Spacebar to jump !
 
-CTRL-C to quit
-=============================
-"""
+LIN_VEL_STEP = 1.0
+ANG_VEL_STEP = 1.0
 
 def getKey():
-    if os.name == 'nt':
-        timeout = 0.1
-        startTime = time.time()
-        while(1):
-            if msvcrt.kbhit():
-                if sys.version_info[0] >= 3:
-                    return msvcrt.getch().decode()
-                else:
-                    return msvcrt.getch()
-            elif time.time() - startTime > timeout:
-                return ''
-
     tty.setraw(sys.stdin.fileno())
     rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
     if rlist:
@@ -42,26 +22,60 @@ def getKey():
 
 if __name__=="__main__":
 
-    settings = termios.tcgetattr(sys.stdin)
-
     rospy.init_node('ball_teleop_key')
-    pub = rospy.Publisher('ball_moving_info', BallMoveInfo, queue_size=10)
-    
+    pub = rospy.Publisher('ball_moving_info', Twist, queue_size=10)
+
+    msg = """
+    =============================
+    Moving the ball around:
+         Z
+    Q    S    D
+        
+    Spacebar to jump !
+
+    CTRL-C to quit
+    =============================
+    """
+
     print(msg)
+
+    settings = termios.tcgetattr(sys.stdin)
+    linear_vel = 0
+    linear_vel_z = 0
+    angular_vel = 0
     while not rospy.is_shutdown():
         key = getKey()
         if key == 'z' :
             print("Z is pressed !")
+            linear_vel = LIN_VEL_STEP
         elif key == 'q' :
             print("Q is pressed !")
+            angular_vel = ANG_VEL_STEP
         elif key == 's' :
             print("S is pressed !")
+            linear_vel = - LIN_VEL_STEP
         elif key == 'd' :
             print("D is pressed !")
+            angular_vel = - ANG_VEL_STEP
         elif key == ' ' :
             print("Space is pressed !")
+            linear_vel_z = LIN_VEL_STEP
+        elif key == 'x' : # Press x to stop
+            print("X is pressed ! \n Ball stopped moving !")
+            linear_vel   = 0.0
+            angular_vel  = 0.0
         else:
             if key == '\x03' : # CTRL-C
                 break
 
-        pub.publish(BallMoveInfo)
+        twist = Twist()
+
+        twist.linear.x = linear_vel
+        twist.linear.y = 0.0
+        twist.linear.z = linear_vel_z
+
+        twist.angular.x = 0.0
+        twist.angular.y = 0.0
+        twist.angular.z = angular_vel
+
+        pub.publish(twist)
